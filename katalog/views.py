78467457fc7c45512531144a78e5bb2053d8d652
AlifioFathanHaryanto <alifio.fathan@ui.ac.id>
2022-09-14 10:10:14 +0700

    kelar

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'nama': 'Alifio Fathan Haryanto',
+        'npm': 2106653483,
+        
+    
+}
+    return render(request, "katalog.html", context)
